f68fe99aa675a79c101da014103165951d5f259d
Patrick Nisble <acereca@outlook.de>
2023-03-12 17:12:50 +0100

    complete pest grammar and sample il file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
-use std::{env, fs, io::Read};
+use std::{env, fs};
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "skill.pest"]
+pub struct SkillParser;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let mut file = fs::File::open(args[1].as_str()).expect("msg");
-    let mut data = String::new();
+    let mut args: Vec<String> = env::args().collect();
+    let data = fs::read_to_string(args[1].as_mut_str()).expect("could not read from file");
 
+
+    let parse = SkillParser::parse(Rule::skill, data.as_str()).expect("ha").next().unwrap();
+    for inner in parse.into_inner() {
+        println!("{:?}: {:?}", inner.as_rule(), inner);
+    }
 }
