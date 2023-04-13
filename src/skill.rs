c059feb4c7a244a6ff5655339e46522a84275ea1
Patrick Nisblé <acereca@outlook.de>
2023-04-13 16:34:30 +0200

    start on parsing pest representation into lsp types

--- src/skill.rs
+++ src/skill.rs
@@ -1,11 +1,17 @@
 use pest::error::Error;
 use pest::iterators::Pair;
 
+use pest::Parser;
+
+use log::info;
+
 #[derive(Parser)]
-#[grammar = "./skill.pest"]
+#[grammar = "skill.pest"]
 pub struct SkillParser;
 
 pub fn parse_skill(path: &str) -> Vec<Pair<Rule>> {
+    let parsed = SkillParser::parse(Rule::skill, path);
+    info!("{:?}", parsed);
     vec![]
 }
 pub fn parse_global_symbols(token: Pair<Rule>) -> Result<&str, Error<Rule>> {
